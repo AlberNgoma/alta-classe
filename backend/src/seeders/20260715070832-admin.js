dd8c047876c4e0e4fe0544a652215bd23df18361
AlberNgoma <albertongoma77@gmail.com>
2026-07-15 08:26:27 +0100

    feat: seeder folder and admin created

---
+++ backend/src/seeders/20260715070832-admin.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+const bcrypt = require("bcrypt")
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const password = "altaClasse2026"
+    const securityPass = await bcrypt.hash(password, (10));
+
+    try {
+      await queryInterface.bulkInsert('profile', [{
+        name: 'admin',
+        email: 'acadmin@gmail.com',
+        password: securityPass,
+        type: 'admin',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }]
+
+      )
+
+      console.log('success, admin created !')
+
+    } catch (error) {
+      console.log(`error !, ${error}`)
+    }
+
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('profile', null, {})
+  }
+};
