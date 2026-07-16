51d4467ce8b818707fd4d34c54de4abf8d865541
AlberNgoma <albertongoma77@gmail.com>
2026-07-16 12:00:06 +0100

    feat : crud of product created

--- backend/src/controllers/product-controller.js
+++ backend/src/controllers/product-controller.js
@@ -26,5 +26,69 @@ module.exports = {
         } catch (error) {
             console.log(`error ! creating product`)
         }
+    },
+
+    async read(req, res) {
+        try {
+
+            const product = await table.product.findAll()
+            return res.status(201).json(product)
+
+        } catch (error) {
+            console.log("error ! cannot posible lister products")
+            res.status(500).json({ error: "cannot posible lister products" })
+        }
+    },
+
+    async update(req, res) {
+        const product_id = req.params.id;
+        const { name, description, price, category, madeIn, picture } = req.body;
+
+        try {
+
+            const product = await table.product.findByPk(product_id);
+            if (!product) return res.status(400).json("this product doesn´t exist");
+
+            const newProduct = await table.product.update(
+                {
+                    name: name,
+                    description: description,
+                    price: price,
+                    category: category,
+                    madeIn: madeIn,
+                    picture: picture
+                },
+
+                {
+                    where: { id: product.id }
+                }
+            )
+
+            return res.status(201).json({ msg: "product updated", product: newProduct })
+
+
+        } catch (error) {
+            console.log(`error : ${error}`);
+            res.status(500).json({ error: "can´t update product!" })
+        }
+
+
+    },
+
+    async delete(req, res) {
+        const product_id = req.params.id;
+
+        try {
+
+            const product = await table.product.findByPk(product_id);
+            if (!product) return res.status(404).json("this product doesn´t exist!")
+            await product.destroy()
+
+            return res.status(201).json("success! product deleted")
+
+        } catch (error) {
+            console.log(`error! ${error}`);
+            res.status(500).json("error! imposible delete product")
+        }
     }
 }
--- backend/src/routes/product-router.js
+++ backend/src/routes/product-router.js
@@ -5,5 +5,8 @@ const upload = require("../config/multer-config");
 const route = express.Router();
 
 route.post('/product', upload.single('picture'), product.create);
+route.get('/product', product.read)
+route.put('/product/:id', upload.single('picture'), product.update)
+route.delete('/product/:id', product.delete)
 
 module.exports = route;
